4d110539812f5a250b688fe423b48c5c70beb7d9
Bhushan Patil <thebhushanp@gmail.com>
2020-09-03 10:52:37 +0530

    Update settings.py

--- openapi_client/settings.py
+++ openapi_client/settings.py
@@ -1,8 +1,8 @@
 host = "https://sbx.kotaksecurities.com/apim"
-access_token = "106f22a7-340e-3386-9dde-f2af9cf3f034"
-userid = "VINAY007";
-consumer_key = "FHibD2Sri95S_SBPLRlJ8ll0vhka";
+access_token = ""
+userid = "";
+consumer_key = "";
 ip = "172.24.2.119";
-app_id = "DQmTest";
-password = "login@1"
-access_code = "1111"
+app_id = "";
+password = "@1"
+access_code = ""
